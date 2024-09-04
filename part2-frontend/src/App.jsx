0570c8cf2a94a186d060876ab6e261b510a20b1b
Nicobrandoni98 <nico.brandoni98@gmail.com>
2024-09-04 17:21:09 -0300

    3

--- part2-frontend/src/App.jsx
+++ part2-frontend/src/App.jsx
@@ -7,7 +7,7 @@ import personsService from "./services/persons";
 import Notification from "./components/Notification";
 
 const App = () => {
-  const [persons, setPersons] = useState(null);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setfilter] = useState("");
