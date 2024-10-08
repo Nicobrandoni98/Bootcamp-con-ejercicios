ca78ef4bba207d4fa2914431a460030ef212b793
Nicobrandoni98 <nico.brandoni98@gmail.com>
2024-10-08 12:33:49 -0300

    ejercicios 4.1 - 4.2

--- part4/App.js
+++ part4/App.js
@@ -1,42 +1,18 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const blogRouters = require ('./controllers/blogs')
+const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
+mongoose.set('strictQuery', false)
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/blogs', blogRouters)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+module.exports = app
--- part4/models/blog.js
+++ part4/models/blog.js
@@ -1,4 +1,10 @@
-import mongoose from 'mongoose';
+const mongoose = require("mongoose");
+
+const mongoUrl = 'mongodb+srv://nicobrandoni98:123qwe@part4ejer.tbgiu.mongodb.net/?retryWrites=true&w=majority&appName=Part4Ejer'
+console.log("Connecting to", mongoUrl);
+
+
+mongoose.connect(mongoUrl)
 
 const blogSchema = new mongoose.Schema({
     title: String,
@@ -6,5 +12,13 @@ const blogSchema = new mongoose.Schema({
     url: String,
     likes: Number
   })
+
+  mongoose.set("toJSON", {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString();
+      delete returnedObject._id;
+      delete returnedObject.__v;
+    },
+  });
   
-  const Blog = mongoose.model('Blog', blogSchema)
+  module.exports = mongoose.model("Blog", blogSchema);
